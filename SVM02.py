735c9602ae96a8ddbf4711a06e32af4dabeae1d0
yola130 <1305451838@qq.com>
2025-02-26 16:45:10 +0800

    Update SVM02.py

--- SVM02.py
+++ SVM02.py
@@ -48,21 +48,21 @@ features = pd.DataFrame(data)
 
 if st.button("Predict"):
     # Predict probabilities
-    predicted_proba = model.predict_proba(features)
-    st.text(predicted_proba)
+    predicted_proba = model.predict_proba(features)[0]
+
     
     # 根据预测概率的最高值来确定预测类别（但这里我们直接根据概率阈值判断）  
     high_risk_threshold = 0.32  # 32% 的阈值  
     if predicted_proba[1] > high_risk_threshold:  # 假设模型输出的第二个概率是高风险类的概率  
-        predicted_class = 1  # Cluster2 
+        predicted_class = 1  # Subphenotype2 
     else:  
-        predicted_class = 0  # Cluster1
+        predicted_class = 0  # Subphenotype1
 
     # 显示预测结果  
-    text = f"Predicted Class: {'*Cluster 2*' if predicted_class == 1 else '*Cluster 1*'}"
+   text = f"Predicted Class: {'*Subphenotype 2*' if predicted_class == 1 else '*Subphenotype 1*'}"
     st.subheader(text, anchor=False)
         
     # 根据预测类别给出建议
-    advice = f"Based on the model, predicted that the probability of Cluster 2 is *{predicted_proba[1] * 100:.1f}%*."
+    advice = f"Based on the model, predicted that the probability of Subphenotype 2 is *{predicted_proba[1] * 100:.1f}%*."
 
     st.subheader(advice, anchor=False)
